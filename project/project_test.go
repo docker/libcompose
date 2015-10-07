f56c33db6396be68aa5fe7dc61950fb4ae97686c
Darren Shepherd <darren@rancher.com>
2015-10-07 01:30:23 -0700

    A blank value for environment should be treated as unset
    This fixes an issue in which the below would not lookup `A` in the
    current environment.
    
    ```yaml
    test:
      image: foo
      environment:
        A:
    ```
    
    Signed-off-by: Darren Shepherd <darren@rancher.com>

--- project/project_test.go
+++ project/project_test.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -107,5 +108,41 @@ func TestParseWithGoodContent(t *testing.T) {
 	err := p.Parse()
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+type TestEnvironmentLookup struct {
+}
+
+func (t *TestEnvironmentLookup) Lookup(key, serviceName string, config *ServiceConfig) []string {
+	return []string{fmt.Sprintf("%s=X", key)}
+}
+
+func TestEnvironmentResolve(t *testing.T) {
+	factory := &TestServiceFactory{
+		Counts: map[string]int{},
+	}
+
+	p := NewProject(&Context{
+		ServiceFactory:    factory,
+		EnvironmentLookup: &TestEnvironmentLookup{},
+	})
+	p.Configs = map[string]*ServiceConfig{
+		"foo": {
+			Environment: NewMaporEqualSlice([]string{
+				"A",
+				"A=",
+				"A=B",
+			}),
+		},
+	}
+
+	service, err := p.CreateService("foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(service.Config().Environment.Slice(), []string{"A=X", "A=X", "A=B"}) {
+		t.Fatal("Invalid environment", service.Config().Environment.Slice())
 	}
 }
