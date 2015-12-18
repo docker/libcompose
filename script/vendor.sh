d893498f4cf57dc2b3dda898ca13a285c57cbf23
Ivan Mikushin <i.mikushin@gmail.com>
2015-12-18 13:24:14 +0500

    Bump test dependencies for nicer unit test errors. Also bump go.
    Signed-off-by: Ivan Mikushin <i.mikushin@gmail.com>

--- script/vendor.sh
+++ script/vendor.sh
@@ -15,7 +15,9 @@ clone git github.com/fsouza/go-dockerclient 39d9fefa6a7fd4ef5a4a02c5f566cb83b73c
 clone git github.com/gorilla/context 215affda49addc4c8ef7e2534915df2c8c35c6cd
 clone git github.com/gorilla/mux f15e0c49460fd49eebe2bcc8486b05d1bef68d3a
 clone git github.com/opencontainers/runc b40c7901845dcec5950ecb37cb9de178fc2c0604
-clone git github.com/stretchr/testify 7e4a149930b09fe4c2b134c50ce637457ba6e966
+clone git github.com/stretchr/testify a1f97990ddc16022ec7610326dd9bce31332c116
+clone git github.com/davecgh/go-spew 5215b55f46b2b919f50a1df0eaa5886afe4e3b3d
+clone git github.com/pmezard/go-difflib d8ed2627bdf02c080bf22230dbb337003b7aba2d
 clone git golang.org/x/crypto 4d48e5fa3d62b5e6e71260571bf76c767198ca02 https://github.com/golang/crypto.git
 clone git golang.org/x/net 3a29182c25eeabbaaf94daaeecbc7823d86261e7 https://github.com/golang/net.git
 clone git gopkg.in/check.v1 11d3bc7aa68e238947792f30573146a3231fc0f1
