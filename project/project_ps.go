066c633b78bd718cd5a6a5f034fd6aea23b4b773
halfa <youngyukari@gmail.com>
2016-10-29 16:26:16 +0900

    Add filtering on project ps
    Signed-off-by: halfa <youngyukari@gmail.com>

--- project/project_ps.go
+++ project/project_ps.go
@@ -1,6 +1,8 @@
 package project
 
 import (
+	"sort"
+
 	"golang.org/x/net/context"
 )
 
@@ -8,6 +10,14 @@ import (
 func (p *Project) Ps(ctx context.Context, services ...string) (InfoSet, error) {
 	allInfo := InfoSet{}
 	for _, name := range p.ServiceConfigs.Keys() {
+		if services != nil { // apply filter
+			sort.Strings(services)
+			index := sort.SearchStrings(services, name)
+			if index > len(services) {
+				continue
+			}
+		}
+
 		service, err := p.CreateService(name)
 		if err != nil {
 			return nil, err
